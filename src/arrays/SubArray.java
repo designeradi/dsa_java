858d2f1a1924092e3a0241757657b6fe112d53c5
Aditya Sahu <sahuaditya083@gmail.com>
2022-07-07 10:35:39 +0530

    print subarray with sum s

--- src/arrays/SubArray.java
+++ src/arrays/SubArray.java
@@ -7,37 +7,45 @@ import java.util.Arrays;
 
 public class SubArray {
     public static void main(String[] args) {
-        List list = Arrays.asList(1,2,3,4,5,6,7,8);
+        List<Integer> list = Arrays.asList(1,2,3,5,7);
 
-        printAllSubArrays(list, list.size());
+        System.out.print(subArraySum(list, list.size(), 12));
     }
 
-    static void printAllSubArrays(List arr, int len){
-        for (int size = 0; size < len; size++) {
-            List arrayList;
-            printMsg("subArr of size: " + (size+1));
+    static ArrayList<Integer> subArraySum(List<Integer> arr, int n, int s)
+    {
+        // Your code here
+        ArrayList<Integer> intList = new ArrayList<>(arr.size());
+        ArrayList<Integer> arrayList = new ArrayList<>();
 
-            for (int j = 0; j < len-size; j++) {
-                arrayList = getSubArr(arr, j, size+j);
-                printArr(arrayList);
+        intList.addAll(arr);
+        loop: for (int size = 0; size < n; size++) {
+            for (int j = 0; j < n-size; j++) {
+                int sum = getSubArrSum(intList, j, size+j);
+                if(sum == s){
+                    arrayList = getSubArr(intList, j, size+j);
+                    break loop;
+                }
             }
-
         }
+        return arrayList;
     }
 
-    static ArrayList<Integer> getSubArr(List arr, int start, int end){
-        ArrayList subArr = new ArrayList<Integer>();
+    static int getSubArrSum(ArrayList<Integer> arr, int start, int end){
+        int sum = 0;
+        for (int i = start; i <= end; i++) {
+            sum += arr.get(i);
+        }
+        return sum;
+    }
+
+    static ArrayList<Integer> getSubArr(ArrayList<Integer> arr, int start, int end){
+        ArrayList<Integer> subArr = new ArrayList<>();
         for (int i = start; i <= end; i++) {
             subArr.add(arr.get(i));
         }
         return subArr;
     }
 
-    static void printMsg(String msg){
-        System.out.println(msg);
-    }
-
-    static void printArr(List arr){
-        System.out.println(arr);
-    }
 }
+
